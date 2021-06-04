4c105a468c01d927ea7aff1db084866e991f43c2
RajoRamarokoto <alivelorajoram@gmail.com>
2021-06-05 00:45:31 +0300

    alexa revision 0.1

---
+++ Alexa.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+
+    try:
+        with sr.Microphone() as source:
+            print('listening')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        print(song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        print(person)
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'single' in command:
+        talk('I am in relationship with the WEB')
+    elif 'joke' in command:
+        joke = pyjokes.get_joke()
+        print(joke)
+        talk(joke)
+
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
